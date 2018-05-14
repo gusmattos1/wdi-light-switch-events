03f421ed66537c5b6b954d8b39531759b4456c37
gusmattos1 <gusmattos1@gmail.com>
2018-05-14 14:43:56 -0400

    assignment done

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 
+var button = document.querySelector('.switch');
+var body = document.querySelector('.light')
+var text = document.querySelector('.status')
 
+button.addEventListener('click', function () {
+
+      if (button.className == "switch on") {
+        button.className = "switch off"
+        body.className = "dark"
+        text.innerText="Hey, who turned off the lights?"      } else {
+        button.className = "switch on"
+        body.className = "light"
+        text.innerText="It's so bright in here!"   
+      }
+
+  });
